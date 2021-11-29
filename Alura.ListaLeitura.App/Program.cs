3903c477cb4fd35b6a3c99a6e00490c1f48ca1f8
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-11-29 17:07:19 -0300

    Adição de Projeto-base corrigido.

--- Alura.ListaLeitura.App/Program.cs
+++ Alura.ListaLeitura.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
 using System;
 
 namespace Alura.ListaLeitura.App
@@ -8,16 +9,11 @@ namespace Alura.ListaLeitura.App
     {
         static void Main(string[] args)
         {
-            var _repo = new LivroRepositorioCSV();
-
-            ImprimeLista(_repo.ParaLer);
-            ImprimeLista(_repo.Lendo);
-            ImprimeLista(_repo.Lidos);
-        }
-
-        static void ImprimeLista(ListaDeLeitura lista)
-        {
-            Console.WriteLine(lista);
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
         }
     }
 }
