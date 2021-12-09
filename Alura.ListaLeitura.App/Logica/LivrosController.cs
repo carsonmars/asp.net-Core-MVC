b0fa25e7d248ded9cedc3ab778d64e6c6c134db6
Carson Souza <carson.m.a.r.souza@gmail.com>
2021-12-09 18:04:13 -0300

    O estágio de Model Binding no request pipeline.

--- Alura.ListaLeitura.App/Logica/LivrosController.cs
+++ Alura.ListaLeitura.App/Logica/LivrosController.cs
@@ -1,18 +1,16 @@
-﻿using Alura.ListaLeitura.App.Html;
+﻿using Alura.ListaLeitura.App.HTML;
 using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App.Logica
 {
-    public class LivrosControllercs
+    public class LivrosController
     {
 
         public static string CarregaLista(IEnumerable<Livro> livros)
